5796000450cfd24386861b42736e4ac862db148b
VRquaeler <VRquaeler@gmx.de>
2022-09-09 00:44:42 +0200

    add shreya offsets

--- ArduPlane/mode_follow.cpp
+++ ArduPlane/mode_follow.cpp
@@ -18,8 +18,13 @@ void ModeFollow::update()
 //    gcs().send_text(MAV_SEVERITY_INFO, "follow update %u", millis());
     Location loc;
     Vector3f vel_ned;
+    Vector3f offsets;
     static uint32_t last_notify;
-    if (plane.g2.follow.get_target_location_and_velocity(loc, vel_ned)) {
+    if ((plane.g2.follow.get_target_location_and_velocity(loc, vel_ned)) && (plane.g2.follow.get_offsets_ned(offsets))) {
+        // apply offsets from follow parameters
+        loc.offset(offsets.x, offsets.y);
+        loc.alt -= offsets.z * 100;
+
         plane.guided_WP_loc = loc;
         const uint32_t now = AP_HAL::millis();
         if (now - last_notify > 10000) {
@@ -27,7 +32,6 @@ void ModeFollow::update()
 //            gcs().send_text(MAV_SEVERITY_INFO, "%u %u %u\n", loc.lat, loc.lng, loc.alt);
         }
         plane.set_guided_WP();
-        plane.adjust_altitude_target();
     }
     plane.calc_nav_roll();
     plane.calc_nav_pitch();
@@ -36,5 +40,6 @@ void ModeFollow::update()
 
 void ModeFollow::navigate()
 {
+    plane.adjust_altitude_target();
     plane.update_loiter(0);
 }
